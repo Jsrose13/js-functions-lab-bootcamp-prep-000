5fb411da461a7d8ce241245b6da2baf63ee01f1a
JESSICA STEVENS <jsrose13@gmail.com>
2017-08-13 04:02:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function happyHolidays(string) {
   return ('Happy holidays!')
 }
 function happyHolidaysTo(name) {
-  console.log(`Happy holidays, ${string}!`)
+  console.log(`Happy holidays, ${name}!`)
 }
